0ad1c7e0ffe5b4db9ab2f2718c061845a801b0fa
Davaughn Williams <65515720+313rdWay@users.noreply.github.com>
2025-03-08 15:30:42 -0500

    Animations/Transitions/Textreadme added

--- SwiftfulThinkingBootcamp/26. AnimationTimingBootcamp.swift
+++ SwiftfulThinkingBootcamp/26. AnimationTimingBootcamp.swift
@@ -9,14 +9,38 @@ import SwiftUI
 
 struct AnimationTimingBootcamp: View {
     @State var isAnimating: Bool = false
+    let timing: Double = 10.0
     var body: some View {
         VStack {
             Button("Button") {
                 isAnimating.toggle()
             }
             RoundedRectangle(cornerRadius: 20.0)
-                .frame(width: isAnimating ? 35 : 50, height: 100)
-                .animation(Animation.default)
+                .frame(width: isAnimating ? 350 : 50, height: 100)
+                .animation(.spring(
+                    // response is basically the same as duration
+                    response: 0.5,
+                    // damping is how we want our object to bounce back from the spring
+                    dampingFraction: 0.7,
+                    blendDuration: 1.0))
+//                .animation(.spring())
+            // Linear goes the same speed the entire animation
+//                .animation(Animation.linear(duration: timing))
+//            
+//            RoundedRectangle(cornerRadius: 20.0)
+//                .frame(width: isAnimating ? 350 : 50, height: 100)
+//            // EaseIn slowly eases into the animation, to normal/fast speed
+//                .animation(Animation.easeIn(duration: timing))
+//            
+//            RoundedRectangle(cornerRadius: 20.0)
+//                .frame(width: isAnimating ? 350 : 50, height: 100)
+//            // EaseInOut goes slow, fast, then slow again
+//                .animation(Animation.easeInOut(duration: timing))
+//            
+//            RoundedRectangle(cornerRadius: 20.0)
+//                .frame(width: isAnimating ? 350 : 50, height: 100)
+//            // EaseOut goes fast then slow
+//                .animation(Animation.easeOut(duration: timing))
         }
     }
 }
