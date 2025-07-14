c4b3f59eece5fd5800027b81d7fe05f69668c130
Davaughn Williams <65515720+313rdWay@users.noreply.github.com>
2025-07-14 18:06:49 -0400

    ContextMenu Bootcamp complete

--- SwiftfulThinkingBootcamp/Bootcamps/34. ContextMenuBootcamp.swift
+++ SwiftfulThinkingBootcamp/Bootcamps/34. ContextMenuBootcamp.swift
@@ -8,8 +8,45 @@
 import SwiftUI
 
 struct ContextMenuBootcamp: View {
+    
+    @State var backgroundColor: Color = Color(#colorLiteral(red: 0.1215686277, green: 0.01176470611, blue: 0.4235294163, alpha: 1))
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(alignment: .leading, spacing: 10.0) {
+            Image(systemName: "house.fill")
+                .font(.title)
+            Text("Swiftful Thinking")
+                .font(.headline)
+            Text("How to use Context Menu")
+                .font(.subheadline)
+        }
+        .foregroundStyle(.white)
+        .padding(30)
+        .background(backgroundColor.cornerRadius(30))
+        .contextMenu(menuItems: {
+            Button(action: {
+                backgroundColor = .yellow
+            }, label: {
+                Label("Share post", systemImage: "flame.fill")
+            })
+            
+            Button {
+                backgroundColor = .red
+            } label: {
+                Text("Report Post")
+            }
+
+            Button {
+                backgroundColor = .green
+            } label: {
+                HStack {
+                    Text("Like post")
+                    Image(systemName: "heart.fill")
+                }
+            }
+
+            
+        })
     }
 }
 
