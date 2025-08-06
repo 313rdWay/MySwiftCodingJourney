151a91328750b5a25b8ef38ef633ea5a1b111266
Davaughn Williams <65515720+313rdWay@users.noreply.github.com>
2025-08-06 18:34:01 -0400

    TextEditorBootcamp Complete

--- SwiftfulThinkingBootcamp/Bootcamps/36. TextEditorBootcamp.swift
+++ SwiftfulThinkingBootcamp/Bootcamps/36. TextEditorBootcamp.swift
@@ -8,8 +8,38 @@
 import SwiftUI
 
 struct TextEditorBootcamp: View {
+    
+    @State var textEditorText: String = "This is the starting text."
+    @State var savedText: String = ""
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView {
+            VStack {
+                TextEditor(text: $textEditorText)
+                    .frame(height: 250)
+                    .foregroundStyle(.black)
+                // This is the only way to change the background color of a TextEditor
+                    .colorMultiply(Color(#colorLiteral(red: 0.8374180198, green: 0.8374378085, blue: 0.8374271393, alpha: 1)))
+                    .cornerRadius(10)
+                Button {
+                    savedText = textEditorText
+                } label: {
+                    Text("Save".uppercased())
+                        .font(.headline)
+                        .foregroundStyle(.white)
+                        .padding()
+                        .frame(maxWidth: .infinity)
+                        .background(Color.blue)
+                        .cornerRadius(10)
+                }
+                
+                Text(savedText)
+                
+                Spacer()
+            }
+            .padding()
+            .navigationTitle("TextEditor Bootcamp")
+        }
     }
 }
 
