3c47a76abcfb33431165a5ad949b8e7b3252ee4b
Davaughn Williams <65515720+313rdWay@users.noreply.github.com>
2025-08-07 17:20:52 -0400

    DatePicker Bootcamp  Complete

--- SwiftfulThinkingBootcamp/Bootcamps/40. DatePickerBootcamp.swift
+++ SwiftfulThinkingBootcamp/Bootcamps/40. DatePickerBootcamp.swift
@@ -8,8 +8,35 @@
 import SwiftUI
 
 struct DatePickerBootcamp: View {
+    
+    @State var selectedDate: Date = Date()
+    
+    let startingDate: Date = Calendar.current.date(from: DateComponents(year: 2018)) ?? Date()
+    let endingDate: Date = Date()
+    
+    var dateFormatter: DateFormatter {
+        let formatter = DateFormatter()
+        formatter.dateStyle = .short
+        formatter.timeStyle = .short
+        return formatter
+    }
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            Text("SELECTED DATE IS:")
+            Text(dateFormatter.string(from: selectedDate))
+                .font(.title)
+            //        DatePicker("Select a Date", selection: $selectedDate)
+            //        DatePicker("Select a date", selection: $selectedDate, displayedComponents: [.date, .hourAndMinute])
+            DatePicker("Select a date", selection: $selectedDate, in: startingDate...endingDate, displayedComponents: [.date])
+            // Now only changes when pressed
+                .accentColor(Color.red)
+                .datePickerStyle(
+                    .compact
+                    //                .graphical
+                    //                .wheel
+                )
+            
+        }
     }
 }
 
