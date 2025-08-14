57e4b4d9c2ff177d8d7eae2e8d2b7b47aed961a3
Davaughn Williams <65515720+313rdWay@users.noreply.github.com>
2025-08-14 14:43:35 -0400

    OnAppear Bootcamp Complete

--- SwiftfulThinkingBootcamp/Bootcamps/46. OnAppearBootcamp.swift
+++ SwiftfulThinkingBootcamp/Bootcamps/46. OnAppearBootcamp.swift
@@ -8,8 +8,37 @@
 import SwiftUI
 
 struct OnAppearBootcamp: View {
+    
+    @State var myText: String = "Starting text."
+    @State var count: Int = 0
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView {
+            ScrollView {
+                Text(myText)
+                LazyVStack {
+                    ForEach(0..<50) { _ in
+                        RoundedRectangle(cornerRadius: 25)
+                            .frame(height: 200)
+                            .padding()
+                            .onAppear {
+                                count += 1
+                            }
+                    }
+                }
+            }
+            .onAppear(perform: {
+                // DispatchQueue is used to add a delay in Swift
+                DispatchQueue.main.asyncAfter(deadline: .now() + 5) {
+                    myText = "This is the new text!"
+                }
+            })
+            // You would use this in a real app if maybe you want function going through a database and wanted them to cancel after the user leaves the view.
+            .onDisappear(perform: {
+                myText = "Ending text,"
+            })
+            .navigationTitle("On Appear \(count)")
+        }
     }
 }
 
