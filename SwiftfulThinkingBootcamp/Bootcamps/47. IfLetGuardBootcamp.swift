2e3502c29eead12e269c0011958f8af0295af0af
Davaughn Williams <65515720+313rdWay@users.noreply.github.com>
2025-08-14 15:28:24 -0400

    If let bootcamp complete

--- SwiftfulThinkingBootcamp/Bootcamps/47. IfLetGuardBootcamp.swift
+++ SwiftfulThinkingBootcamp/Bootcamps/47. IfLetGuardBootcamp.swift
@@ -8,8 +8,59 @@
 import SwiftUI
 
 struct IfLetGuardBootcamp: View {
+    
+    @State var currentUserID: String? = "testUser123"
+    @State var displayText: String? = nil
+    @State var isLoading: Bool = false
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView {
+            VStack {
+                Text("Here we are practing safe coding!")
+                if let text = displayText {
+                    Text(text)
+                        .font(.title)
+                }
+                
+                // DO NOT USE !(FORCE UNWRAP VALUES) EVER BAD CODING PRACTICE
+//                Text(displayText!)
+//                    .font(.title)
+                
+                if isLoading {
+                    ProgressView()
+                }
+                
+                Spacer()
+            }
+            .navigationTitle("Safe Coding")
+            .onAppear {
+                loadData2()
+            }
+        }
+    }
+    
+    func loadData() {
+        if let userID = currentUserID {
+            isLoading = true
+            DispatchQueue.main.asyncAfter(deadline: .now() + 3) {
+                displayText = "This is the new data! User id is: \(userID)"
+                isLoading = false
+            }
+        } else {
+            displayText = "Error, There is no user ID"
+        }
+    }
+    
+    func loadData2() {
+        guard let userID = currentUserID else {
+            displayText = "Error, There is no user ID"
+            return
+        }
+        isLoading = true
+        DispatchQueue.main.asyncAfter(deadline: .now() + 3) {
+            displayText = "This is the new data! User id is: \(userID)"
+            isLoading = false
+        }
     }
 }
 
