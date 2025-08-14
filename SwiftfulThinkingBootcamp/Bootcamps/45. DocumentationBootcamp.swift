41eb87732367c0bcf8465b67f95882e101cf951b
Davaughn Williams <65515720+313rdWay@users.noreply.github.com>
2025-08-14 14:21:16 -0400

    Documentation Bootcamp complete

--- SwiftfulThinkingBootcamp/Bootcamps/45. DocumentationBootcamp.swift
+++ SwiftfulThinkingBootcamp/Bootcamps/45. DocumentationBootcamp.swift
@@ -8,10 +8,73 @@
 import SwiftUI
 
 struct DocumentationBootcamp: View {
+    
+    // MARK: PROPERTIES
+    
+    @State var data: [String] = [
+        "Apples", "Oranges", "Bananas",
+    ]
+
+    @State var showAlert: Bool = false
+    
+    // MARK: BODY
+
+    // DAVAUGHN - Working copy - things to do:
+/*
+     1. Fix title
+     2. Fix alert
+*/
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView { // Start: NAV
+            ZStack {
+                // Background
+                Color.red.ignoresSafeArea()
+                
+                // Foreground
+                foregroundLayer
+                .navigationTitle("Documentation")
+                .navigationBarItems(trailing:
+                                        Button("ALERT", action: {
+                    showAlert.toggle()
+                })
+                )
+                .alert(isPresented: $showAlert) {
+                    getAlert(text: "This is the alert!")
+                }
+            }
+        } // END: NAV
+    }
+    
+    /// This is the foreground layer that holds a scrollView
+    private var foregroundLayer: some View {
+        ScrollView { // START: SCROLLV
+            Text("Hello")
+            ForEach(data, id: \.self) { name in
+                Text(name)
+                    .font(.headline)
+            }
+        } // END: SCROLLV
+
+    }
+    
+    // MARK: FUNCTIONS
+    
+    /// Gets an alert with a specifed title.
+    ///
+    /// This function crates and returns an alert immediately. The alert will have a title based on the text parameter but it will NOT have a message.
+    /// ```
+    /// getAlert(text: "Hi") -> Alert(title: Text("Hi"))
+    /// ```
+    /// - Warning: There is no additional message in this Alert
+    /// - Parameter text: This is the title for the alert.
+    /// - Returns: Returns and alert with a title
+    func getAlert(text: String) -> Alert {
+        return Alert(title: Text(text))
     }
 }
+
+    // MARK: PREVIEW
 
 #Preview {
     DocumentationBootcamp()
